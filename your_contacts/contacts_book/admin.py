8f4032019b6707a0cbeff7d4ac1fc88ca1051800
Ammar Al-Taweel <a.tawil@gmail.com>
2025-02-23 12:38:00 +0000

    Add Contact and Contacts_list definitions
    Contacts_book now has the definitions of Contacts_list and Contact.
    Each user has one Contacts_list. The list is created when the user is
    created.
    Each list has many Contacts.
    This assumes that each user has only 1 Contacts_list. This can be
    modified in the future by adding Contacts_lists that allows the user to
    have ore than one contacts_list (e.g. Business, Family, Clients, etc.).
    Another solution would be to allow the user to create a 'contact type'
    and assign that to each contact.

--- your_contacts/contacts_book/admin.py
+++ your_contacts/contacts_book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contacts_list, Contact
+
+
+class Contact_admin(admin.ModelAdmin):
+     list_display = ('name', 'address', 'telephone_number', 'email' )
+
+admin.site.register(Contacts_list)
+admin.site.register(Contact, Contact_admin)
--- your_contacts/contacts_book/models.py
+++ your_contacts/contacts_book/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Contacts_list(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+    
+class Contact(models.Model):
+    contacts_list = models.ForeignKey(Contacts_list,on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    address = models.TextField(max_length=300, blank=True)
+    telephone_number = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.name
+
+# Create a contact list when the user is created
+@receiver(post_save, sender=User)
+def create_user_contacts_list(sender, instance, created, **kwargs):
+    if created:
+        Contacts_list.objects.create(user=instance)
+
+#Update contacts list when user is updated
+@receiver(post_save, sender=User)
+def save_user_contact_list(sender, instance, **kwargs):
+    instance.contacts_list.save()
